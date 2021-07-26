b3c2b171564802d037f97cc069700fb6896d7d11
unionyy <jadu1028@naver.com>
2021-07-26 20:25:21 +0900

    Knapsack

---
+++ bp-greedy-dp/basic-problems/knapsack/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    freopen("input.txt", "r", stdin);
+    cin >> T;
+    for(int t = 0; t < T; t++) {
+        int N, K;
+        
+    }
+}
