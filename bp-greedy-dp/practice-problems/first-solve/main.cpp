3107b8ac5dad3d008492e65a067a91082c896ae0
unionyy <jadu1028@naver.com>
2021-08-01 23:45:21 +0900

    Solved first solve

--- bp-greedy-dp/practice-problems/first-solve/main.cpp
+++ bp-greedy-dp/practice-problems/first-solve/main.cpp
@@ -1,32 +1,56 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 #define MAXN 1000
 #define MAXF 1000000
+#define INF 2000001
 
 int N;
 int ss[MAXN];
 int fs[MAXN];
-int dels[MAXN];
 
-int FindMax(int time) {
-    int max = -1;
-    int idx = -1;
-    for(int n = 0; n < N; n++) {
-        int del;
-        if(dels[n] < 0) continue;
-        if(time > fs[n]) del = fs[n] - ss[n];
-        else del = time - ss[n];
-        if(del > max) {
-            max = del;
-            idx = n;
-        } else if(del == max && ss[idx] < ss[n]) {
-            max = del;
-            idx = n;
-        }
+int idx[MAXN];
+
+int dp[MAXN + 1][MAXN + 1];
+
+void QuickSort(int l, int r) {
+    if(l >= r) return;
+    int m = (l + r) / 2;
+    int pivotL = l;
+    int pivotR = r;
+    while(true) {
+        while(pivotL < m && fs[idx[pivotL]] <= fs[idx[m]]) pivotL++;
+        while(pivotR > m && fs[idx[pivotR]] >= fs[idx[m]]) pivotR--;
+        if(pivotL == pivotR) break;
+        int tmp = idx[pivotL];
+        idx[pivotL] = idx[pivotR];
+        idx[pivotR] = tmp;
+        if(pivotL == m) m = pivotR;
+        else if(pivotR == m) m = pivotL;
     }
-    dels[idx] = max;
-    return idx;
+    
+    QuickSort(l, m - 1);
+    QuickSort(m + 1, r);
+    return;
+}
+
+int FindD(int _idx, int solved) {
+    if(dp[_idx][solved] != -1) return dp[_idx][solved];
+    if(solved == 0) {
+        dp[_idx][solved] = 0;
+        return 0;
+    }
+    if(_idx == 0) {
+        dp[_idx][solved] = INF;
+        return INF;
+    }
+    int min = FindD(_idx - 1, solved);
+    int b = FindD(_idx - 1, solved - 1) + ss[idx[_idx - 1]];
+    if(min > b) min = b;
+    if(min > fs[idx[_idx - 1]]) min = INF;
+    dp[_idx][solved] = min;
+    return min;
 }
 
 int main() {
@@ -41,18 +65,15 @@ int main() {
         cin >> N;
         for(int n = 0; n < N; n++) {
             cin >> ss[n] >> fs[n];
-            dels[n] = fs[n] - ss[n];
+            idx[n] = n;
         }
+        QuickSort(0, N - 1);
 
-        int time = MAXF;
-        int cnt = 0;
-        while(true) {
-            int idx = FindMax(time);
-            if(idx < 0) break;
-            cnt++;
-            time = dels[idx];
-            dels[idx] = -1;
-        }
+        for(int i = 0; i <= N; i++)
+            for(int j = 0; j <= N; j++) dp[i][j] = -1;
+        for(int i = 0; i <= N; i++) FindD(N, i);
+        int cnt = N;
+        while(dp[N][cnt] == INF) cnt--;
 
         cout << '#' << tc << ' ' << cnt << "\n";
     }
