71b137d90fa18dc5987710d392cd5a80be0acf08
unionyy <jadu1028@naver.com>
2021-08-05 17:48:39 +0900

    Fail: make num

---
+++ heap/basic-problems/make-num/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+int A[10];
+
+struct Node {
+    int left;
+    int cnt;
+} nodes[1000000];
+int nodeCnt;
+
+bool Cmp(Node a, Node b) {
+    if(a.cnt == b.cnt) return a.left < b.left;
+    else return a.cnt < b.cnt;
+}
+
+void Add(int left, int cnt) {
+    nodes[nodeCnt].left = left;
+    nodes[nodeCnt].cnt = cnt;
+    int pivot = nodeCnt;
+    nodeCnt++;
+    while(pivot > 0) {
+        int ppivot = pivot;
+        pivot = pivot - 1 >> 1;
+        if(Cmp(nodes[pivot], nodes[ppivot])) return;
+        nodes[ppivot] = nodes[pivot];
+        nodes[pivot].cnt = cnt;
+        nodes[pivot].left = left;
+    }
+    return;
+}
+
+Node Pop() {
+    Node rev = nodes[0];
+
+    nodes[0] = nodes[--nodeCnt];
+
+    int pivot = 0;
+    while(true) {
+        int ppivot = pivot;
+        pivot << 1;
+        pivot++;
+        if(nodeCnt <= pivot) break;
+        if(pivot + 1 == nodeCnt) {
+            if(Cmp(nodes[pivot], nodes[ppivot])) {
+                Node tmp = nodes[pivot];
+                nodes[pivot] = nodes[ppivot];
+                nodes[ppivot] = tmp;
+            } else break;
+        } else {
+            if(Cmp(nodes[pivot], nodes[ppivot+1])) pivot++;
+            if(Cmp(nodes[pivot], nodes[ppivot])) {
+                Node tmp = nodes[pivot];
+                nodes[pivot] = nodes[ppivot];
+                nodes[ppivot] = tmp;
+            } else break;
+        }
+    }
+    return rev;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    cin >> T;
+    for(int tc = 1; tc <=T; tc++) {
+        int N, K;
+        cin >> N;
+        for(int n = 0; n < N; n++) cin >> A[n];
+        cin >> K;
+
+        int ans;
+        nodeCnt = 0;
+        Add(K, 0);
+        while(nodeCnt > 0) {
+            Node node = Pop();
+            int cnt = node.cnt;
+            int left = node.left;
+            if(left == 0) {
+                ans = cnt;
+                break;
+            }
+            Add(0, cnt + left);
+            for(int n = 0; n < N; n++) {
+                if(left >= A[n]) Add(left / A[n], cnt + left % A[n]);
+            }
+        }
+
+        cout << '#' << tc << ' ' << ans << '\n';
+    }
+}
