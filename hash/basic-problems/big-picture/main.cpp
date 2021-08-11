9e043075b32d282756c861219c889f11e3227f64
unionyy <jadu1028@naver.com>
2021-08-12 00:04:18 +0900

    Solved!! swea Big picture

--- hash/basic-problems/big-picture/main.cpp
+++ hash/basic-problems/big-picture/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define MAXN 2000
-#define HASH_SIZE (1 << 31)
+#define HASH_SIZE (1 << 30)
 #define DIV (HASH_SIZE - 1)
 
 int H, W, N, M;
@@ -10,26 +10,26 @@ int H, W, N, M;
 int myPic[MAXN][MAXN], samPic[MAXN][MAXN];
 int samHash[MAXN][MAXN], tmp[MAXN][MAXN];
 
-int CalcMul(int num) {
+int CalcMul(int num, int shift) {
     unsigned long long rev = 1;
     for(int i = 1; i < num; i++) {
-        rev = (rev << 5) + rev;
+        rev = (rev << shift) + rev;
     }
-    return (int) rev & DIV;
+    return (int) (rev & DIV);
 }
 
-int GetHash(int* piv, int num) {
+int GetHash(int* piv, int num, int shift) {
     unsigned long long hash = 0;
     for(int i = 0; i < num; i++) {
-        hash = (hash << 5) + hash + *piv++;
+        hash = (hash << shift) + hash + *piv++;
     }
-    return (int)hash & DIV;
+    return (int) (hash & DIV);
 }
 
-int GetNext(int prev, int sub, int mul, int add) {
+int GetNext(int prev, int sub, int mul, int add, int shift) {
     unsigned long long hash = prev - (sub * mul);
-    hash = (hash << 5) + hash + add;
-    return (int) hash & DIV;
+    hash = (hash << shift) + hash + add;
+    return (int) (hash & DIV);
 }
 
 int main() {
@@ -53,22 +53,22 @@ int main() {
         }
 
         // Get My Hash
-        for(int i = 0; i < H; i++) tmp[0][i] = GetHash(myPic[i], W);
-        int myHash = GetHash(tmp[0], H);
+        for(int i = 0; i < H; i++) tmp[0][i] = GetHash(myPic[i], W, 4);
+        int myHash = GetHash(tmp[0], H, 5);
 
         // Get Sam Hash
-        int mulC = CalcMul(W);
-        int mulR = CalcMul(H);
+        int mulC = CalcMul(W, 4);
+        int mulR = CalcMul(H, 5);
         for(int i = 0; i < N; i++) {
-            tmp[0][i] = GetHash(samPic[i], W);
+            tmp[0][i] = GetHash(samPic[i], W, 4);
             for(int j = 1; j < M - W + 1; j++) {
-                tmp[j][i] = GetNext(tmp[j-1][i], samPic[i][j-1], mulC, samPic[i][j+W-1]);
+                tmp[j][i] = GetNext(tmp[j-1][i], samPic[i][j-1], mulC, samPic[i][j+W-1], 4);
             }
         }
         for(int i = 0; i < M - W + 1; i++) {
-            samHash[0][i] = GetHash(tmp[i], H);
+            samHash[0][i] = GetHash(tmp[i], H, 5);
             for(int j = 1; j < N - H + 1; j++) {
-                samHash[j][i] = GetNext(samHash[j-1][i], tmp[i][j-1], mulR, tmp[i][j+H-1]);
+                samHash[j][i] = GetNext(samHash[j-1][i], tmp[i][j-1], mulR, tmp[i][j+H-1], 5);
             }
         }
 
